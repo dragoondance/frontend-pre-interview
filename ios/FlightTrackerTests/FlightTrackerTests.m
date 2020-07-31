44a074dd15ab5c079c73904309cca53b713d4d02
Alim Gunawan <dragoondance@gmail.com>
2020-07-31 20:42:53 +0700

    [MOD] setup dependencies and rename project

--- ios/FlightTrackerTests/FlightTrackerTests.m
+++ ios/FlightTrackerTests/FlightTrackerTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BoilerplateTests : XCTestCase
+@interface FlightTrackerTests : XCTestCase
 
 @end
 
-@implementation BoilerplateTests
+@implementation FlightTrackerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
